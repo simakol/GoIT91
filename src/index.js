57f439fd64b40bcfe8204c8cbaa1ee03bd8ebcd3
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-09-13 22:17:35 +0300

    lesson-15

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./css/main.css";
+import "./css/modal.css";
+// import "./scss/style.scss";
+import "./images/img1.jpg";
+import { sum } from "./sum";
+
+console.log("a");
+console.log(sum(5, 99));
