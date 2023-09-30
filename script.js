ea18ff331d246460a7e25446f7e84f277b50ebc3
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-09-30 20:09:55 +0300

    lesson-20

---
+++ script.js
@@ -0,0 +1,16 @@
+// Створи фільмотеку з популярними фільмами, для цього використай
+// https://developer.themoviedb.org/reference/trending-movies
+
+// Щоб отримати постер фільму потрібно підставити url з відповіді від бекенду та url з документації
+// https://developer.themoviedb.org/docs/image-basics
+
+// Відмалюй картки з фільмами
+// Приклад картки  => https://prnt.sc/Hi_iLLg7Nd1F
+
+// Реалізуй пагінацію за допомогою кнопки "Load More"
+
+// *********************** Кнопка "Load More" ************************** \\
+
+// 92a9a9e3708a3e9451b7037d5906879a - API KEY
+// https://api.themoviedb.org/3 - BASE URL
+// trending/movie/week - endpoint
